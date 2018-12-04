3fcb466363169f8758e0e8b3ce8202f131204afc
Stanislav Sergeev <stas.webdev@gmail.com>
2018-12-05 04:32:26 +0500

    gulp tasks for playground

--- gulpfile.js
+++ gulpfile.js
@@ -8,34 +8,61 @@ const reload = browserSync.reload;
 const baseDir = __dirname;
 const port = 3000;
 
-gulp.task('default', done => runSequence('browserSync', 'build', 'watch', done));
-
-gulp.task('watch', () => {
-  gulp.watch(baseDir + '/*.pug', ['pug']);
-  gulp.watch(baseDir + '/*.sass', ['sass']);
-});
+gulp.task('default', done => runSequence(
+  'browserSync',
+  ['index:build', 'playground:build'],
+  ['index:watch', 'playground:watch'],
+  done
+));
 
 gulp.task('browserSync', () => {
-  browserSync.init(['*.html', '*.css', '*.js'], {
-   server: {
-     baseDir: baseDir
-   },
-   port: port,
-   open: true,
-   notify: false
- });
+  browserSync.init([
+    '*.html', '*.css', '*.js',
+    'playground/**/*.html', 'playground/**/*.css', 'playground/**/*.js'
+  ], {
+    server: {
+      baseDir: baseDir
+    },
+    port: port,
+    open: true,
+    notify: false
+  });
 });
 
-gulp.task('build', ['pug', 'sass']);
+gulp.task('index:watch', () => {
+  gulp.watch(baseDir + '/*.pug', ['index:pug']);
+  gulp.watch(baseDir + '/*.sass', ['index:sass']);
+});
 
-gulp.task('pug', () => {
+gulp.task('index:build', ['index:pug', 'index:sass']);
+
+gulp.task('index:pug', () => {
   return gulp.src(baseDir + '/*.pug')
     .pipe(pug({ pretty: true }))
     .pipe(gulp.dest(baseDir));
 });
 
-gulp.task('sass', () => {
+gulp.task('index:sass', () => {
   return gulp.src(baseDir + '/*.sass')
     .pipe(sass({ outputStyle: 'expanded' }))
     .pipe(gulp.dest(baseDir));
+});
+
+gulp.task('playground:watch', () => {
+  gulp.watch(baseDir + '/playground/**/*.pug', ['pug']);
+  gulp.watch(baseDir + '/playground/**/*.sass', ['sass']);
+});
+
+gulp.task('playground:build', ['playground:pug', 'playground:sass']);
+
+gulp.task('playground:pug', () => {
+  return gulp.src(baseDir + '/playground/**/*.pug')
+    .pipe(pug({ pretty: true }))
+    .pipe(gulp.dest(baseDir + '/playground'));
+});
+
+gulp.task('playground:sass', () => {
+  return gulp.src(baseDir + '/playground/**/*.sass')
+    .pipe(sass({ outputStyle: 'expanded' }))
+    .pipe(gulp.dest(baseDir + '/playground'));
 });
